aac1f26a17edaed368466fc646e70aff27103d52
zhouliang-yu <120040077@link.cuhk.edu.cn>
2022-01-07 23:30:33 +0800

    update create_ac_dataset.jl

--- create_ac_dataset.jl
+++ create_ac_dataset.jl
@@ -1,49 +1,66 @@
-# read the matpower data and process it in PowerModels.jl
-using PowerModels, Gurobi, Ipopt, Random, Distributions;
+using PowerModels, Ipopt, Gurobi
 using CSV;
 using DataFrames;
 
-list_file = readdir("./data/hard_case/Julia_Solvable");
-result_cat = Dict();
+list_file = readdir("./data/hard_case/Julia_Solvable")
+data_path = "./data/hard_case/Julia_Solvable"
+solver = optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6)
+case_name = AbstractString[]
+case_obj = Float64[]
+case_solve_time = Float64[]
+case_ter_status = TerminationStatusCode[]
+case_dual_status = ResultStatusCode[]
+case_primal_status = ResultStatusCode[]
+case_obj_lb = Float64[]
 ErrorList = [];
-NumList = [];
-OptList = [];
-InfeasList = [];
-name = [];
-ter_status = [];
+result_ter_cat = Dict();
+
+
+
 
 for itema in list_file
-    #    if occursin("pglib",itema)
-    try
+    try 
         if occursin("case", itema)
-            # result = run_dc_opf("./data/hard_case/"*itema,Gurobi.Optimizer);
+            # result = run_opf(data_path * itema, ACPPowerModel, solver)
             result = run_ac_opf("./data/hard_case/Julia_Solvable/" * itema, Ipopt.Optimizer)
-            result_cat[itema] = result["termination_status"]
+            push!(case_name, itema)
+            push!(case_solve_time, result["solve_time"])
+            push!(case_ter_status, result["termination_status"])
+            push!(case_primal_status, result["primal_status"])
+            push!(case_dual_status, result["dual_status"])
+            push!(case_obj, result["objective"])
+            push!(case_obj_lb, result["objective_lb"])
+
+            result_ter_cat[itema] = result["termination_status"]
         end
     catch
         push!(ErrorList, itema)
     end
 end
 
-# obtain the numerical issue cases
-for iKey in keys(result_cat)
-    if result_cat[iKey] == NUMERICAL_ERROR
-        push!(NumList, iKey)
-    end
-end
-# numerical issues are mostly caused by infeasibility as well
+#for Gurobi solver
+# # obtain the numerical issue cases
+# for iKey in keys(result_ter_cat)
+#     if result_ter_cat[iKey] == NUMERICAL_ERROR
+#         push!(NumList, iKey)
+#     end
+# end
+# # numerical issues are mostly caused by infeasibility as well
 
-for iKey in keys(result_cat)
-    if result_cat[iKey] == OPTIMAL
-        push!(OptList, iKey)
-    end
-end
+# for iKey in keys(result_ter_cat)
+#     if result_ter_cat[iKey] == OPTIMAL
+#         push!(OptList, iKey)
+#     end
+# end
 
-for iKey in keys(result_cat)
-    if result_cat[iKey] == INFEASIBLE
-        push!(InfeasList, iKey)
-    end
-end
+# for iKey in keys(result_ter_cat)
+#     if result_ter_cat[iKey] == INFEASIBLE
+#         push!(InfeasList, iKey)
+#     end
+# end
 
-df_ter_status = DataFrame(result_cat)
-CSV.write("ter_status.csv", df_ter_status)
+dict_cat = Dict("case_name"=>case_name, "solve_time" => case_solve_time, "termination_status" => case_ter_status, "dual_status" => case_dual_status,
+    "primal_status" => case_primal_status, "objective" => case_obj, "objective_lb" => case_obj_lb)
+
+df_result = DataFrame(dict_cat) 
+CSV.write("result.csv", df_result)
