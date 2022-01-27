598809b1e8877365d68e73b673c0551833e89f30
zhouliang-yu <120040077@link.cuhk.edu.cn>
2022-01-27 16:45:29 +0800

    update ac_perturb

--- ac_perturb.jl
+++ ac_perturb.jl
@@ -7,88 +7,73 @@ addprocs(20);
 @everywhere using DataFrames;
 
 
-list_file = readdir("./data");
-
-list_file_al = [
-    "case1888rte.m"
-    "case6515rte.m"
-    "case89pegase.m"
-    "case1354pegase.m"
-    "pglib_opf_case2746wp_k__api.m"
-    "pglib_opf_case24_ieee_rts__api.m"
-    "case6470rte.m"
-    
-];
+list_file = readdir("./data/");
+perturbed_file = readdir("./data/perturb/");
 
 perturbResult_dict = Dict();
 run_time_dict = Dict();
 inf_case = Dict() #the infeasible perturb case
-model_name = []
-run_time_list = []
-inf_case_list = []
+
 
 println("Number of processes: ", nprocs())
 println("Number of workers: ", nworkers())
 
 
 for itema in list_file
-
     if occursin(".m", itema)
-        if (itema in list_file_al) == false
-            network_data = parse_file("./data/" * itema)
-            load_keys = [i for i in keys(network_data["load"])]
-            gen_keys = [i for i in keys(network_data["gen"])]
-            σ_load = Dict()
-            load_distrn = Dict()
-            
-            for i in 1:length(load_keys)
-                if network_data["load"][load_keys[i]]["pd"] != 0
-                    σ_load[load_keys[i]] = abs(network_data["load"][load_keys[i]]["pd"] * 0.03)
-                    load_distrn[load_keys[i]] = Normal(0, σ_load[load_keys[i]])
-                else
-                    σ_load[load_keys[i]] = 0
-                    load_distrn[load_keys[i]] = Uniform(0, 0.0000001)
-                end
+    
+        model_name = []
+        run_time_list = []
+        inf_case_list = []
+        ter_status_itema = []
+        run_time_itema = []
+        load_info_itema = []
+        pd_list_itema = []
+        obj = []
+    
+        network_data = parse_file("./data/" * itema)
+        load_keys = [i for i in keys(network_data["load"])]
+        gen_keys = [i for i in keys(network_data["gen"])]
+        σ_load = Dict()
+        load_distrn = Dict()
+        for i in 1:length(load_keys)
+            if network_data["load"][load_keys[i]]["pd"] != 0
+                σ_load[load_keys[i]] = abs(network_data["load"][load_keys[i]]["pd"] * 0.03)
+                load_distrn[load_keys[i]] = Normal(0, σ_load[load_keys[i]])
+            else
+                σ_load[load_keys[i]] = 0
+                load_distrn[load_keys[i]] = Uniform(0, 0.0000001)
             end
-        
-        
-            perturbResult_dict[itema] = pmap(ω -> run_Ipopt_para_ac(network_data, load_distrn, load_keys, gen_keys), 1:1000)
-        
-            ter_status_itema = []
-            run_time_itema = []
-            load_info_itema = []
-        
-            for profile in perturbResult_dict[itema]
-                if typeof(profile[1]) != Float64
-                    #push to infeasible_list
-                    # push!(infeasible_list, (i, σ_load[load_keys[i]], load_distrn[load_keys[i]]))
-                
-                    # infeasible_load_dict = Dict("index" => i, "σ_load" => σ_load[load_keys[i]], "load_distrn" => load_distrn[load_keys[i]])
-                
-                    push!(ter_status_itema, profile[1])
-                    push!(load_info_itema, profile[4])
-                end
-            
-                push!(run_time_itema, profile[3])
-            end
-        
-            run_time_mean = mean(run_time_itema)   # mean of running time
-            run_time_stddev = std(run_time_itema)  # standard deviation of running time
-            run_time_dict[itema] = Dict("run_time_mean" => run_time_mean, "run_time_stddev" => run_time_stddev)
-            push!(run_time_list, run_time_dict[itema])
-        
-        
-            inf_case[itema] = Dict("ter_status" => ter_status_itema, "load_info_itema" => load_info_itema)
-            push!(inf_case_list, inf_case[itema])
-            push!(model_name, itema)
         end
+    
+    
+    
+        perturbResult_dict[itema] = pmap(ω -> run_Ipopt_para_ac(network_data, load_distrn, load_keys, gen_keys), 1:1000)
+        for profile in perturbResult_dict[itema]
+            pd_dict_profile = Dict()
+            #push to infeasible_list
+            # push!(infeasible_list, (i, σ_load[load_keys[i]], load_distrn[load_keys[i]]))
+    
+            # infeasible_load_dict = Dict("index" => i, "σ_load" => σ_load[load_keys[i]], "load_distrn" => load_distrn[load_keys[i]])
+            push!(model_name, itema)
+            push!(obj, profile[1])
+            push!(ter_status_itema, profile[2])
+            push!(run_time_itema, profile[4])
+    
+            pd_keys = [i for i in keys(profile[5])]
+            for keys in pd_keys
+                # store the pd information
+                # push!(pd_list_itema, profile[4][keys]["pd"])
+                pd_dict_profile[keys] = profile[5][keys]["pd"]
+            end
+            push!(pd_list_itema, pd_dict_profile)
+        end
+        result = Dict("case_name" => model_name, "pd" => pd_list_itema, "objective value" => obj,
+            "ter_status" => ter_status_itema, "run_time" => run_time_itema)
+    
+        result = DataFrame(result)
+        CSV.write("./data/perturb/" * itema[1:end-2] * "_"*"perturb" * ".csv", result, bufsize = 2^30)
+    
     end
-
 end
 
-
-result_cat_dict = Dict("case_name" => model_name, "run_time_distribution" => run_time_list,
-    "infeasible_list" => inf_case_list)
-
-df_result = DataFrame(result_cat_dict)
-CSV.write("result_perturb_cat.csv", df_result, bufsize = 2^30)
